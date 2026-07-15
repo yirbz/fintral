759ee84345a15a32e4de8a1aa232a4634a483a0a
Yirber Yunier Mañon <yunielherrera2008@gmail.com>
2026-07-15 13:43:08 -0400

    fix: add port 8000 to railway.ts networking config

--- .railway/railway.ts
+++ .railway/railway.ts
@@ -28,7 +28,7 @@ export default defineRailway(() => {
     root: "backend",
     build: { buildEnvironment: "V3", builder: "DOCKERFILE", dockerfilePath: "Dockerfile" },
     replicas: { "us-east4-eqdc4a": 1 },
-    domains: [],
+    networking: { port: 8000 },
     env: {
       ADMIN_EMAIL: preserve(),
       ADMIN_FULL_NAME: preserve(),
